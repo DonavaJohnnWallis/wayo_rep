d917a32f7b96219ce2a662bd4ffa2b83d5bde244
Donavan <Donavanwallis@hotmail.com>
2018-02-08 20:25:15 +0200

    contract added

--- app/src/main/java/com/vf/admin/vf_rep/ContractThreeActivity.java
+++ app/src/main/java/com/vf/admin/vf_rep/ContractThreeActivity.java
@@ -2,47 +2,25 @@ package com.vf.admin.vf_rep;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.TextView;
-import android.widget.Switch;
 import android.widget.CompoundButton;
+import android.widget.Switch;
 
-public class ContractOneActivity extends AppCompatActivity {
+public class ContractThreeActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_contract_one);
-
-        TextView storetext = (TextView) findViewById(R.id.storenametext);
-        TextView addresstext = (TextView) findViewById(R.id.roadtext);
-        TextView potext = (TextView) findViewById(R.id.poboxtext);
-        Bundle bu;
-        bu=getIntent().getExtras();
-        storetext.setText(bu.getString("storename"));
-        addresstext.setText(bu.getString("address"));
-        potext.setText(bu.getString("pobox"));
-
-
-
+        setContentView(R.layout.layout_contract_three);
 
         //Get widgets reference from XML layout
 
         Switch sButton = (Switch) findViewById(R.id.switch_btn);
         Button btn = (Button) findViewById(R.id.button3);
         btn.setEnabled(false);
-
-
-
 
 //Set a CheckedChange Listener for Switch Button
         sButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -65,11 +43,7 @@ public class ContractOneActivity extends AppCompatActivity {
 
     public void next(View view) {
 
-
-
-
-        Intent intent = new Intent(ContractOneActivity.this, ContractTwoActivity.class);
-
+        Intent intent = new Intent(ContractThreeActivity.this, ContractFourActivity.class);
 
         startActivity(intent);
     }
--- app/src/main/java/com/vf/admin/vf_rep/StoreDetailsActivity.java
+++ app/src/main/java/com/vf/admin/vf_rep/StoreDetailsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class StoreDetailsActivity extends AppCompatActivity {
 
@@ -14,8 +15,15 @@ public class StoreDetailsActivity extends AppCompatActivity {
     }
 
     public void OpenContract(View view) {
+        EditText storename = findViewById(R.id.storename);
+        EditText address = findViewById(R.id.address);
+        EditText pobox = findViewById(R.id.pobox);
 
         Intent intent = new Intent(StoreDetailsActivity.this, ContractOneActivity.class);
+
+        intent.putExtra("storename",storename.getText().toString());
+        intent.putExtra("address",address.getText().toString());
+        intent.putExtra("pobox",pobox.getText().toString());
 
         startActivity(intent);
     }
